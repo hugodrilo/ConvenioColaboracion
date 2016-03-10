95efdf49cbb60fbeb073e21d9bc1a3f58eb2dd24
hugodrilo <hugodrilo@hotmail.com>
2016-03-10 16:26:21 -0600

    Agregados objetos de partes y compromisos para convenio

--- ConvenioColaboracion.Client/app/convenios/controllers/convenioCtrl.js
+++ ConvenioColaboracion.Client/app/convenios/controllers/convenioCtrl.js
@@ -5,14 +5,17 @@
 (function () {
     "use strict";
 
-    // Convenio controller
+    // Definicion de modulo y controlador
     angular
         .module("convenioColaboracion")
         .controller("ConvenioCtrl", ["$scope", "$uibModal", ConvenioCtrl]);
 
+    // Controlador de convenio
     function ConvenioCtrl($scope, $uibModal, convenioResource) {
         var vm = this;
         vm.convenio = {};
+        vm.convenio.partes = [];
+        vm.convenio.compromisos = [];
         vm.message = "";
 
         //Agregado calendario con estilo de gobmx
@@ -32,14 +35,23 @@
 
         // Ventana modal para agregar partes
         vm.toggleModalParte = function () {
+
+            $scope.entidad = {};
+
             var modalInstance = $uibModal.open({
                 animation: $scope.animationsEnabled,
                 templateUrl: "app/convenios/templates/parte.html",
-                controller: "ModalInstanceCtrl"
+                controller: "ModalInstanceCtrl",
+                resolve: {
+                    entidad: function () {
+                        return $scope.entidad;
+                    }
+                }
             });
 
-            modalInstance.result.then(function () {
-                console.log("algo");
+            modalInstance.result.then(function (data) {
+                console.log(data);
+                vm.convenio.partes.push(data);
             }, function () {
                 console.log("Modal dismissed at: " + new Date());
             });
@@ -47,37 +59,48 @@
 
         // Ventana modal para agregar compromisos
         vm.toggleModalCompromiso = function () {
+
+            $scope.entidad = {};
+
             var modalInstance = $uibModal.open({
                 animation: $scope.animationsEnabled,
                 templateUrl: "app/convenios/templates/compromiso.html",
-                controller: "ModalInstanceCtrl"
+                controller: "ModalInstanceCtrl",
+                resolve: {
+                    entidad: function () {
+                        return $scope.entidad;
+                    }
+                }
             });
 
-            modalInstance.result.then(function () {
-                console.log("algo");
+            modalInstance.result.then(function (data) {
+                console.log(data);
+                vm.convenio.compromisos.push(data);
             }, function () {
                 console.log("Modal dismissed at: " + new Date());
+                console.log();
+
             });
         };
     };
 
-    // Modal Instance controller
+    // The Modal Instance controller
     angular
        .module("convenioColaboracion")
-       .controller("ModalInstanceCtrl", function ($scope, $uibModalInstance) {
+       .controller("ModalInstanceCtrl", function ($scope, $uibModalInstance, entidad) {
+           $scope.entidad = entidad;
 
            //Agregado calendario con estilo de gobmx
            $(".calendarioGobMx").datepicker();
 
-           console.log("Depende");
-
+           // Boton de aceptar para la ventana modal.
            $scope.ok = function () {
-               console.log("Ok");
+               $uibModalInstance.close($scope.entidad);
            };
 
+           // Boton para cancelar para la ventana modal.
            $scope.cancel = function () {
-               console.log("Cancel");
-               $uibModalInstance.dismiss("cancel");
+               $uibModalInstance.dismiss("cancelar");
            };
        });
 })();
